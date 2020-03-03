f0afbde640b18fb18026b7abff94cd48aab6b034
Md Yaseen Ahmed <55386528+mdyaseenahmed@users.noreply.github.com>
2020-03-04 01:27:44 +0530

    Update Recursive_Linear_&_Binary_Search.C

--- ADA_Lab_Assignment/Recursive_Linear_&_Binary_Search.C
+++ ADA_Lab_Assignment/Recursive_Linear_&_Binary_Search.C
@@ -5,6 +5,7 @@ Repeat the experiment for different values of N  and plot a graph of the time ta
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 
 //C Function For Recursive Linear Search..!!
 int Linear_Search(int A[],int l,int r,int key)
@@ -36,49 +37,69 @@ int main()
 	int A[1000000],k;
 	int n,i;
 	int choice;
+	clock_t start,end;
+	double time_required;
 	printf("\nThe Choices Are : \n1.Linear Search.\n2.Binary Search.\nEnter Your Choice : ");
 	scanf("%d",&choice);
 	switch(choice)
 	{
 		case 1 : 	printf("\nEnter The Size Of Array : ");
-					    scanf("%d",&n);
-					    printf("\nEnter %d Elements : ",n);
-					    for(i=0;i<n;i++)
-				  	  {
-  		  				scanf("%d",&A[i]);
+					scanf("%d",&n);
+					printf("\nEnter %d Elements : ",n);
+					for(i=0;i<n;i++)
+				    	{
+  		  			    A[i] = rand() % 100000+1;
   	  				}
+  	  				printf("\nRandomly Generate Array is : \n");
+					for(i=0;i<n;i++)
+					{
+					    printf("%d\n",A[i]);
+					}
     					printf("\nEnter The Key To Be Searched : ");
-					    scanf("%d",&k);
-					    int flag = Linear_Search(A,0,n-1,k);
-					    if(flag != -1)
-					    {
-					      printf("\nElement %d Found At A[%d] Index...!",k,flag);	
-					    }
-					    else
-					    {
-						    printf("\nElement %d Not Found..!",k);
-					    }
-					    break;
-					
+					scanf("%d",&k);
+					start = clock();
+                    			int flag = Linear_Search(A,0,n-1,k);
+                    			end = clock();
+                    			time_required  = ((double)(end - start))/CLOCKS_PER_SEC;
+					if(flag != -1)
+                    			{
+                        			printf("\nElement %d Found At A[%d] Index...!",k,flag);
+                        			printf("\nTime Required to Search %d Element is %f.\n",k,time_required);
+					}
+					else
+					{
+					printf("\nElement %d Not Found..!",k);
+					}
+					break;
+				
 		case 2 :	printf("\nEnter The Size Of Array : ");
-		    			scanf("%d",&n);
-				    	printf("\nEnter %d Elements : ",n);
-					    for(i=0;i<n;i++)
-					    {
-						    scanf("%d",&A[i]);
-					    }
-					    printf("\nEnter The Key To Be Searched : ");
-					    scanf("%d",&k);
-					    flag = Bin_Search(A,0,n-1,k);
-					    if(flag == -1)
-					    {
-						    printf("\nELement %d Not Found..!",k);
-					    }
-					    else
-					    {
-						    printf("\nElement %d Found At A[%d] Index...!",k,flag);
-					    }
-					    break;
+		    		scanf("%d",&n);
+				printf("\nEnter %d Elements : ",n);
+				for(i=0;i<n;i++)
+				{
+					A[i] = rand() % 100000+1;
+				}
+				printf("\nRandomly Generate Array is : \n");
+				for(i=0;i<n;i++)
+				{
+				    printf("%d\n",A[i]);
+				}
+				printf("\nEnter The Key To Be Searched : ");
+				scanf("%d",&k);
+				start = clock();
+					flag = Bin_Search(A,0,n-1,k);
+				end = clock();
+				time_required = ((double)(end - start))/CLOCKS_PER_SEC;
+				if(flag == -1)
+				{
+				    printf("\nELement %d Not Found..!",k);
+				}
+				else
+				{
+				    printf("\nElement %d Found At A[%d] Index...!",k,flag);
+				    printf("\nTime Required to Search %d Element is %f.\n",k,time_required);
+                    		}
+				break;
 					
 		default :	printf("\nInvalid Choice...!");			
       				exit(0);		
